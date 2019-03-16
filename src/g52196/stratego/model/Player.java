28ac016cabc100f0cc4eb3669bbca86d8af4026a
52196 <52196@etu.he2b.be>
2019-03-16 20:08:00 +0100

    fix Player constructor to throw an error if the color passed in parameter is null

--- src/g52196/stratego/model/Player.java
+++ src/g52196/stratego/model/Player.java
@@ -2,6 +2,7 @@ package g52196.stratego.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author 52196
@@ -20,8 +21,12 @@ public class Player {
      * the list of pieces.
      * 
      * @param color
+     * @throws NullPointerException if color equals null
      */
     public Player(PlayerColor color) {
+        if (color == null) {
+            throw new NullPointerException("The color can't be null");
+        }
         this.color = color;
         this.pieces = new ArrayList<>();
     }
@@ -48,5 +53,34 @@ public class Player {
      */
     public void addPiece(Piece piece) {
         pieces.add(piece);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + Objects.hashCode(this.color);
+        hash = 23 * hash + Objects.hashCode(this.pieces);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (this.color != other.color) {
+            return false;
+        }
+        if (!Objects.equals(this.pieces, other.pieces)) {
+            return false;
+        }
+        return true;
     }
 }
