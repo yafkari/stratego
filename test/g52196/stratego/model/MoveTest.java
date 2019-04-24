29b3dae7c77aa755c5034ef947696b9e922e3717
52196 <52196@etu.he2b.be>
2019-04-24 23:58:16 +0200

    add tests for Move

--- test/g52196/stratego/model/MoveTest.java
+++ test/g52196/stratego/model/MoveTest.java
@@ -1,9 +1,34 @@
 package g52196.stratego.model;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class MoveTest {
-    
+
+    @Test
+    public void testGetPiece() {
+        System.out.println("testGetPiece");
+        Move instance = new Move(new Piece(0, PlayerColor.RED), 
+                new Position(0, 0), new Position(0, 0));
+        assertEquals(new Piece(0, PlayerColor.RED), instance.getPiece());
+    }
+
+    @Test
+    public void testGetStart() {
+        System.out.println("testGetStart");
+        Move instance = new Move(new Piece(0, PlayerColor.RED), 
+                new Position(0, 0), new Position(0, 0));
+        assertEquals(new Position(0, 0), instance.getStart());
+    }
+
+    @Test
+    public void testGetEnd() {
+        System.out.println("testGetEnd");
+        Move instance = new Move(new Piece(0, PlayerColor.RED), 
+                new Position(0, 0), new Position(0, 0));
+        assertEquals(new Position(0, 0), instance.getEnd());
+    }
+
     @Test(expected = NullPointerException.class)
     public void testMoveWhenNull() {
         System.out.println("testMoveWhenNull");
