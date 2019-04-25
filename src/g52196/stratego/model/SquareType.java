ce1e8d11cbc66a5155ee5ba3a3dda6929063ffd6
Afkari Younes 52196 <g52196@PDG.ESI.HEB.SITE>
2019-04-25 09:00:50 +0200

    --Start iteration 3--
    add SquareType enum

---
+++ src/g52196/stratego/model/SquareType.java
@@ -0,0 +1,12 @@
+package g52196.stratego.model;
+
+/**
+ * Represents the type of a square
+ * 
+ * The square can be of type LAND or WATER
+ * 
+ * @author g52196
+ */
+public enum SquareType {
+    LAND, WATER;
+}
