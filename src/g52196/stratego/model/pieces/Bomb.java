0f3be9e3898cce1649b64e84ac935ac5f26bcd97
52196 <52196@etu.he2b.be>
2019-04-25 22:58:06 +0200

    add Bomb model

---
+++ src/g52196/stratego/model/pieces/Bomb.java
@@ -0,0 +1,21 @@
+package g52196.stratego.model.pieces;
+
+import g52196.stratego.model.Piece;
+import g52196.stratego.model.PlayerColor;
+
+/**
+ * @author 52196
+ *
+ * Represents a bomb
+ */
+public class Bomb extends Piece {
+
+    /**
+     * Creates a bomb with a color passed in parameter
+     * 
+     * @param color the color of the bomb
+     */
+    public Bomb(PlayerColor color) {
+        super(11, color);
+    }
+}
