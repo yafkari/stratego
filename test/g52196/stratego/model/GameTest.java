c1b5ce946e231f845b1b4dab393a176d3c8dedce
52196 <52196@etu.he2b.be>
2019-05-08 22:17:49 +0200

    add test to GameTest

--- test/g52196/stratego/model/GameTest.java
+++ test/g52196/stratego/model/GameTest.java
@@ -307,6 +307,29 @@ public class GameTest {
 
         assertArrayEquals(board.getSquares(), result);
     }
+    
+    @Test
+    public void testApplyWhenIsNotStronger() {
+        System.out.println("testApplyWhenIsNotStronger");
+        Game instance = new Game();
+        instance.initialize();
+        instance.select(4, 4);
+        instance.apply(instance.getMoves().get(0));
+
+        Square[][] result = instance.getBoard();
+        Board board = new Board();
+        
+        board.getSquares()[0][1].put(new Flag(RED));
+        board.getSquares()[3][2].put(new General(RED));
+        board.getSquares()[4][2].put(new Flag(BLUE));
+        board.getSquares()[4][1].put(new General(BLUE));
+        board.getSquares()[0][0].put(new Bomb(RED));
+        board.getSquares()[1][0].put(new Miner(BLUE));
+        board.getSquares()[3][4].put(new Spy(BLUE));
+        board.getSquares()[4][0].put(new Pathfinder(RED));
+
+        assertArrayEquals(board.getSquares(), result);
+    }
 
     @Test
     public void testApplyWhenDemining() {
